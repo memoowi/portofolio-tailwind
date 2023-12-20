b9b972fab275f1000069c7fbd5a04f6688795b02
memoowi <megatenlike@gmail.com>
2023-12-20 14:20:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins' : ['Poppins', 'sans-serif'],
+      'teko' : ['Teko', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
